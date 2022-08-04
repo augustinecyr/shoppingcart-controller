0700c3ebd741c376546d45497fc4a4abf351a706
augustinecyr <augustinecyr@gmail.com>
2022-08-04 13:30:47 +0800

    name appears

--- src/main/java/com/sg/shoppingcart/controllers/CartController.java
+++ src/main/java/com/sg/shoppingcart/controllers/CartController.java
@@ -1,12 +1,28 @@
 package com.sg.shoppingcart.controllers;
 
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping(path="/")
+@RequestMapping("/cart")
 public class CartController {
-  
+
+    @PostMapping
+	public String postCart(@RequestBody MultiValueMap<String, String> form
+				, Model model) {
+
+		String name = form.getFirst("name");
+		if ((null == name) || (name.trim().length() <= 0))
+			name = "anonymous";
+
+		model.addAttribute("name", name.toUpperCase());
+
+		return "cart";
+	}
+
 }
