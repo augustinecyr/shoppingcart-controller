1051e6419bd1e12da8666b648c97c2405f883ebf
augustinecyr <augustinecyr@gmail.com>
2022-08-05 11:20:56 +0800

    server-session method

--- src/main/java/com/sg/shoppingcart/services/CartService.java
+++ src/main/java/com/sg/shoppingcart/services/CartService.java
@@ -3,13 +3,11 @@ package com.sg.shoppingcart.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sg.shoppingcart.repositories.CartRepository;
 
 @Service
 public class CartService {
 
-    @Autowired
-    private CartRepository cartRepo;
 
-    
+
+
 }
