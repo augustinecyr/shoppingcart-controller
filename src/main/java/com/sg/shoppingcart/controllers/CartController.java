2f0e9fa7d74ceaf93598f4b6af923c60ab17cf0d
augustinecyr <augustinecyr@gmail.com>
2022-08-05 09:31:53 +0800

    hidden form fields version

--- src/main/java/com/sg/shoppingcart/controllers/CartController.java
+++ src/main/java/com/sg/shoppingcart/controllers/CartController.java
@@ -1,5 +1,8 @@
 package com.sg.shoppingcart.controllers;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,26 +13,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sg.shoppingcart.services.CartService;
 
-
 @Controller
 @RequestMapping("/cart")
 public class CartController {
 
-    @Autowired
-    private CartService cartSvc;
+	@Autowired
+	private CartService cartSvc;
 
-
-    @PostMapping
-	public String postCart(@RequestBody MultiValueMap<String, String> form
-				, Model model) {
+	@PostMapping
+	public String postCart(@RequestBody MultiValueMap<String, String> form, Model model) {
 
 		String name = form.getFirst("name");
-		if ((null == name) || (name.trim().length() <= 0))
+		if (isNull(name))
 			name = "anonymous";
 
+		String c = form.getFirst("contents");
+		List<String> cart = new LinkedList<>();
+		if (!isNull(c))
+			cart = cartSvc.deserialize(c);
+
+		String item = form.getFirst("item");
+		if (!isNull(item))
+			cart.add(item);
+
+		model.addAttribute("displayName", name);
+		model.addAttribute("contents", cartSvc.serialize(cart));
+		model.addAttribute("cart", cart);
+		model.addAttribute("empty", cart.isEmpty());
 		model.addAttribute("name", name);
 
 		return "cart";
+	}
+
+	private boolean isNull(String s) {
+		return ((null == s) || s.trim().length() <= 0);
 	}
 
 }
--- src/main/java/com/sg/shoppingcart/services/CartService.java
+++ src/main/java/com/sg/shoppingcart/services/CartService.java
@@ -1,11 +1,23 @@
 package com.sg.shoppingcart.services;
 
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
-
-
 
 @Service
 public class CartService {
 
+    // Takes a CSV and convert to a List<String>
+    public List<String> deserialize(String s) {
+        String[] items = s.split(",");
+        List<String> contents = new LinkedList<>();
+        for (String i : items)
+            contents.add(i);
+        return contents;
+    }
+
+    public String serialize(List<String> c) {
+        return String.join(",", c);
+    }
 }
