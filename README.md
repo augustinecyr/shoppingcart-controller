2936a8e915c84ea0b3f8d6ac63af00cd78c0dbdf
augustinecyr <augustinecyr@gmail.com>
2022-08-05 12:08:05 +0800

    cleanyellow errors

---
+++ README.md
@@ -0,0 +1,13 @@
+## Shopping Cart Exercise
+### Using 3 different ways of implementation to store user input
+
+### Method 01 - Hidden Form Fields
+1. navigate to branch "hidden-form-fields"
+
+
+### Method 02 - Utilising Redis to store the data
+1. navigate to branch "redis-method"
+
+
+### Method 03 - Storing the contents in Client Browser aka "Cookies"
+2. navigate to branch "server-sesh"
--- src/main/java/com/sg/shoppingcart/controllers/CartController.java
+++ src/main/java/com/sg/shoppingcart/controllers/CartController.java
@@ -1,6 +1,6 @@
 package com.sg.shoppingcart.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
@@ -8,15 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sg.shoppingcart.services.CartService;
+
 
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
 
-    @Autowired
-    private CartService cartSvc;
+
 
 
     @PostMapping
